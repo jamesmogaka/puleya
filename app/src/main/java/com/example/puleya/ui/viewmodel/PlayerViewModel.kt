e3696d94e0af2129f691d19a44ca5df37c5ffa25
mogaka <jamesoyondi23@gmail.com>
2025-04-19 17:19:40 +0300

    Create PlayerViewModel with HiltViewModel annotation.

---
+++ app/src/main/java/com/example/puleya/ui/viewmodel/PlayerViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.puleya.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class PlayerViewModel(
+
+):ViewModel() {
+}
